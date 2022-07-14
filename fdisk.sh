c04a721e9cba5839fdf5cdf673c8fc1b602eff5f
saltq <91617568+saltq144@users.noreply.github.com>
2022-07-14 18:01:57 +0300

    added comments to fdisk.sh

--- fdisk.sh
+++ fdisk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk hdimage.iso
-	g
-	n
-	
-		
-	
-	t
-	1
-	w
+	g # gpt
+	n # new partition
+	  # default number
+	  # default start
+	  # default end
+	t # switch partition type (partition because only partition)
+	1 # type 1
+	w # write
 EOF
 
