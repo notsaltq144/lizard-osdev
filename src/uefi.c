3da8531f65908cca0490294c422302e6eabfd3ec
saltq <91617568+saltq144@users.noreply.github.com>
2022-08-08 11:27:38 +0300

    Removed unused functions

--- src/uefi.c
+++ src/uefi.c
@@ -108,44 +108,6 @@ int main(void) {
 	return 0;
 }
 
-/*
- * The following code is licensed unfer CC BY-SA 2.5
- * Source: https://stackoverflow.com/a/784455/19418319
- * The name of the function have been modified without endorsement from the original source. The coding style has been modified without endorsement from the original source.
- * Written by GManNickG (https://stackoverflow.com/users/87234/gmannickg)
- * Modified by saltq144 on github (https://github.com/saltq144)
- * A function that reverses a string.
- * (c) GManNickG 2009
- */
-void strrev(char *str) {
-	/* skip null */
-	if (str == 0) {
-		return;
-	}
-
-	/* skip empty string */
-	if (*str == 0) {
-		return;
-	}
-
-	/* get range */
-	char *start = str;
-	char *end = start + strlen(str) - 1; /* -1 for \0 */
-	char temp;
-
-	/* reverse */
-	while (end > start) {
-		/* swap */
-		temp = *start;
-		*start = *end;
-		*end = temp;
-
-		/* move */
-		++start;
-		--end;
-	}
-}
-
 int charToBcd(int x) {
 	/* i have nothing to say, just visualize ret wot and x while running this. it works */
 	int ret = 0;
@@ -175,16 +137,6 @@ askUserContinue_ask:
 	goto askUserContinue_ask;
 }
 
-char *specializedShortToString(char *buffer, short x) {
-	char *nullAdr = buffer;
-	while (x) {
-		*nullAdr++ = '0' + (x % 10);
-		x /= 10;
-	}
-	*nullAdr = 0;
-	strrev(buffer);
-	return nullAdr;
-}
 void apitest(void) {
 	printf("apitest: called\n");
 	return;
