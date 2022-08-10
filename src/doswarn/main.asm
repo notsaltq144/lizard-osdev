cc0dfd508161f4d67df2c2432a2b0db77d62c95b
saltq <91617568+saltq144@users.noreply.github.com>
2022-08-10 14:06:46 +0300

    Changed doswarn message

--- src/doswarn/main.asm
+++ src/doswarn/main.asm
@@ -20,35 +20,19 @@ stableSegment:
 	int $0x10
 
 	mov $string, %si
-	call print
-
-	xor %bx,   %bx
-	mov $0x03, %ah
+	mov $0x0e, %ah
+loop:
+	lodsb
+	cmp $0x00, %al
+	je end
 	int $0x10
-	inc %dh
-	mov $0,    %dl
-	mov $0x02, %ah
-	int $0x10
-
-	mov $string2, %si
-	call print
-
+	jmp loop
+end:
 	mov $0x01,   %ah
 	mov $0x0706, %cx
 	int $0x10
 
 halt:	jmp halt
 
-print:
-	mov $0x0e, %ah
-printLoop:
-	lodsb
-	cmp $0x00, %al
-	je printEnd
-	int $0x10
-	jmp printLoop
-printEnd:
-	ret
-string:  .asciz "This disk is only bootable on UEFI devices."
-string2: .asciz "Switch to UEFI mode or buy another computer."
+string:  .asciz "firmware type error"
 
